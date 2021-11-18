e46f70686213b48316266e8da5ccc9341f20f7c5
matto0211 <mateusz.wozniak.0211@gmail.com>
2021-11-18 03:43:52 -0800

    Adapted several variables to be more useful in the face of connecting to databases.

--- Main.py
+++ Main.py
@@ -1,27 +1,31 @@
 from bs4 import BeautifulSoup as Soup
 import requests
 
+url = 'https://www.efortuna.pl/zaklady-bukmacherskie/pilka-nozna/ekstraklasa-polska'
+
 
 class Event:
     def __init__(self, name, date, odds):
-        self.name = name
+        n = name.strip().split(" - ")
+        self.host = n.__getitem__(0)
+        self.visitor = n.__getitem__(1)
         self.date = date
         self.odds = odds
 
     def check_arbitrage(self):
         bet1_0_2 = 0.0
-        for i in range(0,3):
-            bet1_0_2 += 1/float(self.odds.__getitem__(i).text)
-        bet1_02 = 1/float(self.odds.__getitem__(0).text) + 1/float(self.odds.__getitem__(4).text)
-        bet0_12 = 1/float(self.odds.__getitem__(1).text) + 1/float(self.odds.__getitem__(5).text)
-        bet2_01 = 1/float(self.odds.__getitem__(2).text) + 1/float(self.odds.__getitem__(3).text)
+        for i in range(0, 3):
+            bet1_0_2 += 1 / float(self.odds.__getitem__(i).text)
+        bet1_02 = 1 / float(self.odds.__getitem__(0).text) + 1 / float(self.odds.__getitem__(4).text)
+        bet0_12 = 1 / float(self.odds.__getitem__(1).text) + 1 / float(self.odds.__getitem__(5).text)
+        bet2_01 = 1 / float(self.odds.__getitem__(2).text) + 1 / float(self.odds.__getitem__(3).text)
 
-        arb_list = [bet1_0_2,bet1_02,bet0_12,bet2_01]
-        arb_list = [round(100*(1.0 - val), 2) for val in arb_list]
+        arb_list = [bet1_0_2, bet1_02, bet0_12, bet2_01]
+        arb_list = [round(100 * (1.0 - val), 2) for val in arb_list]
         print(arb_list)
 
     def to_str(self):
-        print(self.name + self.date)
+        print(self.host + " - " + self.visitor + "\n" + self.date)
         oddsRows = ""
         for enum, x in enumerate(self.odds):
             oddsRows += (x.text + " ")
@@ -29,12 +33,12 @@ class Event:
                 print(oddsRows)
                 oddsRows = ""
         self.check_arbitrage()
-        print("=====================")
+        print("=====================\n")
 
 
 events_list = []
 
-site_contents = requests.get('https://www.efortuna.pl/zaklady-bukmacherskie/pilka-nozna/ekstraklasa-polska')
+site_contents = requests.get(url)
 site = Soup(site_contents.text, 'lxml')
 games = site.find('tbody')
 
