548c5abeae050cc36fb3f80d245dfcc0ab5353b5
matto0211 <mateusz.wozniak.0211@gmail.com>
2021-11-18 01:58:55 -0800

    Test version of retrieving and processing odds of football game outcomes. Extended the previous version by ability to process data for several games at once.

--- Main.py
+++ Main.py
@@ -1,22 +1,39 @@
 from bs4 import BeautifulSoup as Soup
 import requests
 
+
+class Event:
+    def __init__(self, name, date, odds):
+        self.name = name
+        self.date = date
+        self.odds = odds
+
+    def to_str(self):
+        print(self.name + self.date)
+        oddsRows = ""
+        for enum, x in enumerate(self.odds):
+            oddsRows += (x.text + " ")
+            if enum == 2 or enum == 5:
+                print(oddsRows)
+                oddsRows = ""
+        print("=====================")
+
+
+events_list = []
+
 site_contents = requests.get('https://www.efortuna.pl/zaklady-bukmacherskie/pilka-nozna/ekstraklasa-polska')
 site = Soup(site_contents.text, 'lxml')
-games = site.findAll('tbody')
+games = site.find('tbody')
 
-for game in games:
-    name = (game.find('span', class_='market-name')).text
-    date = (game.find('span', class_='event-datetime')).text
-    print(name + date)
-    odds = game.findAll('span', class_='odds-value')
+names = games.findAll('span', class_='market-name')
+dates = games.findAll('span', class_='event-datetime')
+oddsAll = games.findAll('span', class_='odds-value')
 
-    oddsFormat = ""
+for e,name in enumerate(names):
+    oddsList = []
+    for x in range (6*e, 6*e+6):
+        oddsList.append(oddsAll.__getitem__(x))
+    newEvent = Event(name.text,dates.__getitem__(e).text,oddsList)
+    events_list.append(newEvent)
 
-    for enum, x in enumerate(odds):
-        oddsFormat += (x.text + " ")
-        if enum == 2 or enum == 5:
-            print(oddsFormat)
-            oddsFormat = ""
-
-    print("===========================")
+    events_list.__getitem__(e).to_str()
